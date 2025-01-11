4ea65fb00d404a9292490876e19e0eff0d349a17
dvirbz <dvirbz@gmail.com>
2025-01-11 16:55:38 +0200

    added lstm_AE_cls

--- lstm_AE.py
+++ lstm_AE.py
@@ -10,3 +10,16 @@ class LSTM_AE(nn.Module):
         z, _ = self.encoder(x)
         x_hat, _ = self.decoder(z)
         return x_hat
+    
+class LSTM_AE_Classifier(nn.Module):
+    def __init__(self, input_size, hidden_size, number_classes, bidirectional=True):
+        super().__init__()
+        self.encoder = nn.LSTM(input_size, hidden_size, batch_first=True, bidirectional=bidirectional)
+        self.decoder = nn.LSTM(hidden_size * 2 if bidirectional else hidden_size, input_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size * 2 if bidirectional else hidden_size, number_classes)
+        
+    def forward(self, x):
+        z, _ = self.encoder(x)
+        x_hat, _ = self.decoder(z)
+        cls = self.fc(z)
+        return x_hat, cls
